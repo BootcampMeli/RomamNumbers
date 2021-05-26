2300e0a017f37f196bf357dfe764d1ad5e221963
giosantoro-meli <giovanni.santoro@mercadolivre.com>
2021-05-26 11:39:24 -0300

    Files

---
+++ src/main/java/com/example/first/controllers/FirstController.java
@@ -0,0 +1,22 @@
+package com.example.first.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class FirstController {
+
+    @GetMapping()
+    public String cumprimento(){
+        return "Olá!";
+    }
+
+    @GetMapping("/{nome}")
+    public String cumprimento(@PathVariable String nome){
+        return "Olá, " + nome;
+    }
+}
+
